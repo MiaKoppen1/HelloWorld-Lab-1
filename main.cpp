24e8321d49d84220b936f4bd1222000cf1e2f16e
mia <koppen11@tritons.iowacentral.edu>
2023-09-11 10:51:52 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/* Mia Koppen
+3rd semester, 1st year
+Due: September 15
+Lab #1
+Outputing hello world
+*/
+
+
+#include <iostream>
+int main()
+{
+	system("color 4");
+	std::cout << "Hello World!";
+
+	return 0;
+}
